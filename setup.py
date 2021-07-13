09bb62a12bde3985e1e2ef8335917b48bfbc4f42
Sebastian Velandia <svelandia@Sebastians-MacBook-Pro.local>
2021-07-12 21:41:24 -0500

    creating pgbackup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pgbackup',
+    version='0.1',
+    description='Database backup locally or aws s3',
+    author='svelandia',
+    install_requires=[],
+    packages=find_packages()
+)
